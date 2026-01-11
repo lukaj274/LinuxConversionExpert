b532318bfd8ca0b27841893319c408b52768db28
Luka Jacenko <jluka8082@gmail.com>
2026-01-11 15:46:52 -0800

    added page to help with installation of other distros

---
+++ LinuxConversionExpert/Views/InstallOtherDistroView.axaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace LinuxConversionExpert.Views;
+
+public partial class InstallOtherDistroView : UserControl
+{
+    public InstallOtherDistroView()
+    {
+        InitializeComponent();
+    }
+
+    private void Back_OnClick(object? sender, RoutedEventArgs e)
+    {
+        // Reset the padding and switch to the InstallDistroView page
+        var installView = new InstallDistroView();
+        Padding = installView.Padding;
+        Content = installView;
+    }
+}
