d65762b1658e4619e9ece2a92d3e5c4884aa33fd
lukaj <jluka8082@gmail.com>
2026-05-19 18:16:32 -0700

    added page to show how to install software

---
+++ LinuxConversionExpert/Views/InstallSoftwareView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LinuxConversionExpert.Views;
+
+public partial class InstallSoftwareView : UserControl
+{
+    public InstallSoftwareView()
+    {
+        InitializeComponent();
+    }
+}
